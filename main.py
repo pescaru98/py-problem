ee6b0928662b2e173b65988333a212578f99241d
pescaru98 <mihai.pescaru@elysian-software.com>
2023-11-08 17:15:45 +0200

    refactor is_digit_valid

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def is_digit_repeated(digit, number):
     return number.count(digit) > 1
 
 #Check if the first digit is not zero
-def is_digit_valid(pos, new_digit):
-    if int(pos) == 0 and int(new_digit) == 0:
+def is_digit_valid(string_number):
+    if int(string_number[0]) == 0:
         return False
     return True
 
@@ -30,12 +30,12 @@ def is_digit_valid(pos, new_digit):
 def get_unique_digit(i, number):
     copy_of_number = number
 
-    if not is_digit_repeated(copy_of_number[i], copy_of_number) and is_digit_valid(i, copy_of_number[i]):
+    if not is_digit_repeated(copy_of_number[i], copy_of_number) and is_digit_valid(copy_of_number):
         return copy_of_number[i]
     
     new_digit = copy_of_number[i]
 
-    while is_digit_repeated(new_digit, copy_of_number) or not is_digit_valid(i, new_digit):
+    while is_digit_repeated(new_digit, copy_of_number) or not is_digit_valid(copy_of_number):
         new_digit = generate_new_digit()
 
         copy_of_number = get_replace_string_character_at(i, new_digit, copy_of_number)
@@ -57,5 +57,4 @@ def generate_different_digit_number():
     initial_generated_number = generate_initial_number()
 
     return get_final_generated_number(initial_generated_number)
-    
     
